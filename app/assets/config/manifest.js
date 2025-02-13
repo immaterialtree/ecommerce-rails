a291c98aee39596e78dbb367d2fdbd1eb9b04c90
Naoki <naokirafael.miura@gmail.com>
2025-02-13 13:16:08 -0300

    feat(assets): atualizar manifest.js e adicionar application.js com dependências

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
-//= link bootstrap.min.js
-//= link popper.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+  //= link application.js
+//= link bootstrap.js
+//= link @popperjs--core.js
