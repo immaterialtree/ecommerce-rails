ea33de9016692c8157db11b8f4c11d318f4d63e9
Naoki <naokirafael.miura@gmail.com>
2025-02-13 13:15:25 -0300

    feat(carts): garantir que o carrinho não seja nulo e calcular o total

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,13 @@
 class CartsController < ApplicationController
   def show
-    @cart = cart_products
+    @cart = cart_products || []
+    @total = @cart.sum(&:price)
   end
 
   def add_item
     session[:cart] ||= []
     if Product.exists?(params[:item])
-        session[:cart] << params[:item]
+    session[:cart] << params[:item]
         notice = "Item adicionado ao carrinho."
     else
         notice = "Item não encontrado."
@@ -21,23 +22,48 @@ class CartsController < ApplicationController
   end
 
   def pay
-    product_counts = (session[:cart] || []).each_with_object(Hash.new(0)) do |prod_id, counts|
-      counts[prod_id.to_s] += 1
+    require 'mercadopago'
+
+    # Integração com Mercado Pago
+    sdk = Mercadopago::SDK.new('6597116855860817')
+    payment_data = {
+      transaction_amount: @total,
+      token: params[:token],
+      description: 'Descrição do pagamento',
+      installments: 1,
+      payment_method_id: 'visa',
+      payer: {
+        email: 'payer_email@example.com'
+      }
+    }
+    payment_response = sdk.payment.create(payment_data)
+    payment = payment_response[:response]
+
+    if payment[:status] == 'approved'
+      clear_cart
+      redirect_to root_path, notice: "Pagamento realizado com sucesso."
+    else
+      redirect_to cart_path, alert: "Falha no pagamento: #{payment[:status_detail]}"
     end
-    products = Product.where(:id.in => product_counts.keys)
-    products.each do |product|
-      count = product_counts[product.id.to_s] || 0
-      product.update(stock: product.stock - count)
-    end
-    session[:cart] = [] # limpa o carrinho após pagamento
-    redirect_to root_path, notice: "Pagamento realizado com sucesso."
   end
 
   private
   def cart_products
     product_ids = session[:cart] || []
-    existing_products = Product.where(id: product_ids)
-    session[:cart] = existing_products.pluck(:id) # atualiza a sessão com apenas os produtos existentes
+    existing_products = Product.where(:_id.in => product_ids)
+    session[:cart] = existing_products.pluck(:id).map(&:to_s) # atualiza a sessão com apenas os produtos existentes
     existing_products
+  end
+
+  private
+  def clear_cart
+    product_counts = (session[:cart] || []).each_with_object(Hash.new(0)) do |prod_id, counts|
+      counts[prod_id.to_s] += 1
+    end
+    products = Product.where(:_id.in => product_counts.keys)
+    products.each do |product|
+      count = product_counts[product.id.to_s] || 0
+      product.update(stock: product.stock - count)
+    end
   end
 end
