08be3582500bf868cc956b103b3780ba66dd4ed0
Naoki Miura <naokirafael.miura@gmail.com>
2025-02-20 10:25:36 -0300

    feat(seeds): adicionar nomes e imagens aleatórias aos produtos no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
+nouns = ["Cadeira", "Mesa", "Computador", "Livro", "Caneta"]
+adjectives = ["Super", "Grande", "Rápido", "Novo", "Antigo"]
+images = [
+  "https://cdn-icons-png.freepik.com/256/6935/6935960.png?semt=ais_hybrid",
+  "https://cdn-icons-png.flaticon.com/512/3573/3573045.png",
+  "https://cdn-icons-png.flaticon.com/512/3159/3159066.png"
+]
+
 5.times do |i|
-    Product.create!(name: "Product ##{i}", description: "A product.", price: 39.90 + i*10, stock: 50 - i, image: "https://cdn-icons-png.freepik.com/256/6935/6935960.png?semt=ais_hybrid")
-  end
-  puts "Produtos criados"
+  name = "#{nouns.sample} #{adjectives.sample}"
+  Product.create!(name: name, description: "A product.", price: 39.90 + i*10, stock: 50 - i, image: images.sample)
+end
+puts "Produtos criados"
